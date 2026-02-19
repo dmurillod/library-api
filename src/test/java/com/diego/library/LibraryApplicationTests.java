953f08a0f2c86225339438773ac1d7fe31717a25
Diego Murillo <diegoa.murillod@gmail.com>
2026-02-19 15:33:54 -0500

    📦 chore: implement feature-based modular package structure

---
+++ src/test/java/com/diego/library/LibraryApplicationTests.java
@@ -0,0 +1,15 @@
+package com.diego.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
