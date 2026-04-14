effd90ba3f93eacff88763c1c53aeb0d7de933cb
Diego Murillo <diegoa.murillod@gmail.com>
2026-04-13 20:40:46 -0500

    ✨ feat: integrate sipscan client for receipt generation

--- src/main/java/com/diego/library/config/WebClientConfig.java
+++ src/main/java/com/diego/library/config/WebClientConfig.java
@@ -11,10 +11,20 @@ public class WebClientConfig {
     @Value("${services.receipts.url}")
     private String receiptsServiceUrl;
 
+    @Value("${services.erp.url}")
+    private String erpServiceUrl;
+
     @Bean
     public WebClient receiptsWebClient() {
         return WebClient.builder()
                 .baseUrl(receiptsServiceUrl)
+                .build();
+    }
+
+    @Bean
+    public WebClient erpWebClient() {
+        return WebClient.builder()
+                .baseUrl(erpServiceUrl)
                 .build();
     }
 }
