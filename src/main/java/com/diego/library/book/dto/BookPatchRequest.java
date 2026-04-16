2a1aaf800a64ae90bd63e341104de6f4d13d69b0
Diego Murillo <diegoa.murillod@gmail.com>
2026-04-15 20:10:39 -0500

    ✨ feat: add patch endpoint for partial book update

---
+++ src/main/java/com/diego/library/book/dto/BookPatchRequest.java
@@ -0,0 +1,32 @@
+package com.diego.library.book.dto;
+
+public class BookPatchRequest {
+
+    private String title;
+    private String author;
+    private String isbn;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
