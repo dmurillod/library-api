04f003a86898a07665c0ca1bbcf23a61c2201cdf
Diego Murillo <diegoa.murillod@gmail.com>
2026-04-27 20:30:11 -0500

    ✨ feat: add delete books by pqr id for cross-ms data sync

--- src/main/java/com/diego/library/book/repository/BookRepository.java
+++ src/main/java/com/diego/library/book/repository/BookRepository.java
@@ -3,6 +3,7 @@ package com.diego.library.book.repository;
 import com.diego.library.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -10,4 +11,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByIsbn(String isbn);
 
     boolean existsByIsbn(String isbn);
+
+    List<Book> findByPqrId(String pqrId);
 }
